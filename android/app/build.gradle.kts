fe8a4dfd0810b60bcde3fe5ac8be8b63e9b134ff
Phillip Chin <ekkus93@gmail.com>
2026-06-22 05:52:16 -0700

    chore(android): bump version to 0.3.2 (versionCode 5)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.phillipchin.webrtctunnel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
         testInstrumentationRunner = "com.phillipchin.webrtctunnel.TestTunnelRunner"
     }
 
